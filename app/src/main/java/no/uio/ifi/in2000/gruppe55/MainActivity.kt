789cf8bb16781ab0e57cabf1cb32975fb68d3dcc
Erik Skaalhegg <eriksklhgg@gmail.com>
2019-04-04 18:07:30 +0200

    Linked working basic toolbar plus resource files to nav component

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
@@ -3,7 +3,9 @@ package no.uio.ifi.in2000.gruppe55
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v7.widget.Toolbar
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         val navController = findNavController(this, R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        //link navigation controller and application bar with toolbar
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
+        //link navigation controller with the bottom navigation menu
         findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
