c0c5b3e7bb96bc96dbb9b6b16fa3ef0ef5074e21
Erik Skaalhegg <eriksklhgg@gmail.com>
2019-04-04 14:06:17 +0200

    Added navigation components from jetpack, plus temp files blankFragments

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
@@ -2,11 +2,19 @@ package no.uio.ifi.in2000.gruppe55
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
