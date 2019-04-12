1fbfb4e03c663b18db37fae5c4f064c961bb531b
julianho <julianho@student.matnat.uio.no>
2019-04-13 01:56:49 +0200

    Bedre indentering på predefinerte stasjonmodeller.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/AirqualityforecastModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/AirqualityforecastModel.kt
@@ -7,57 +7,58 @@ import android.arch.lifecycle.ViewModel
 class AirqualityforecastModel : ViewModel() {
     private val mutableStationList: MutableLiveData<List<StationModel>> by lazy {
         val data = MutableLiveData<List<StationModel>>()
-        data.value = ArrayList<StationModel>() +
-                StationModel(
-                    height = 0.3,
-                    name = "Porsgrunn",
-                    delomrade = null,
-                    eoi = null,
-                    grunnkrets = null,
-                    kommune = null,
-                    latitude = null,
-                    longitude = null
-                ) +
-                StationModel(
-                    height = 1.4,
-                    name = "Bergen",
-                    delomrade = null,
-                    eoi = null,
-                    grunnkrets = null,
-                    kommune = null,
-                    latitude = null,
-                    longitude = null
-                ) +
-                StationModel(
-                    height = 2.2,
-                    name = "Trondheim",
-                    delomrade = null,
-                    eoi = null,
-                    grunnkrets = null,
-                    kommune = null,
-                    latitude = null,
-                    longitude = null
-                ) +
-                StationModel(
-                    height = 3.4,
-                    name = "Oslo",
-                    delomrade = null,
-                    eoi = null,
-                    grunnkrets = null,
-                    kommune = null,
-                    latitude = null,
-                    longitude = null
-                ) +
-                StationModel(
-                    height = 5.3,
-                    name = "Skien",
-                    delomrade = null,
-                    eoi = null,
-                    grunnkrets = null,
-                    kommune = null,
-                    latitude = null,
-                    longitude = null
-                )
+        data.value =
+            ArrayList<StationModel>() +
+            StationModel(
+                height = 0.3,
+                name = "Porsgrunn",
+                delomrade = null,
+                eoi = null,
+                grunnkrets = null,
+                kommune = null,
+                latitude = null,
+                longitude = null
+            ) +
+            StationModel(
+                height = 1.4,
+                name = "Bergen",
+                delomrade = null,
+                eoi = null,
+                grunnkrets = null,
+                kommune = null,
+                latitude = null,
+                longitude = null
+            ) +
+            StationModel(
+                height = 2.2,
+                name = "Trondheim",
+                delomrade = null,
+                eoi = null,
+                grunnkrets = null,
+                kommune = null,
+                latitude = null,
+                longitude = null
+            ) +
+            StationModel(
+                height = 3.4,
+                name = "Oslo",
+                delomrade = null,
+                eoi = null,
+                grunnkrets = null,
+                kommune = null,
+                latitude = null,
+                longitude = null
+            ) +
+            StationModel(
+                height = 5.3,
+                name = "Skien",
+                delomrade = null,
+                eoi = null,
+                grunnkrets = null,
+                kommune = null,
+                latitude = null,
+                longitude = null
+            )
         data
     }
 
