874dce0e4bacbf2ae353e4c8dcd0a51cdbe46590
julianho <julianho@student.matnat.uio.no>
2019-04-13 18:42:22 +0200

    Station display is now sorted alphabetically.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/ListFragment.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/ListFragment.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.gruppe55
 
+import android.annotation.TargetApi
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_list.*
 import no.uio.ifi.in2000.gruppe55.viewmodel.airqualityforecastModel
+import java.util.*
+import kotlin.Comparator
 
 class ListFragment : Fragment() {
 
@@ -17,6 +20,7 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    @TargetApi(24)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -34,7 +38,10 @@ class ListFragment : Fragment() {
             { stationMap ->
                 eListe.elementer.clear()
 
-                for ((station, location) in stationMap ?: HashMap()) {
+                // TODO (julianho): Comparator should handle null gracefully.
+                val comparator: Comparator<StationModel> = Comparator { s, t -> s.name!!.compareTo(t.name!!) }
+
+                for ((station, location) in stationMap?.toSortedMap(comparator) ?: TreeMap()) {
                     // TODO (julianho): Extract variables from the current time, not the first in the day.
                     val aqi = location?.data?.time?.first()?.variables?.aqi?.value
                     eListe.elementer.add(Element(station.name, aqi))
