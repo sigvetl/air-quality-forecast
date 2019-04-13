3cbfc990487607bae530d2a7c3f2b56c27507b3e
julianho <julianho@student.matnat.uio.no>
2019-04-13 19:00:46 +0200

    Generelle mindre endringer (som f.eks. senke antall imports og bli kvitt midlertidig TargetApi.)

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/ListFragment.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/ListFragment.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.in2000.gruppe55
 
-import android.annotation.TargetApi
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -9,8 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_list.*
 import no.uio.ifi.in2000.gruppe55.viewmodel.airqualityforecastModel
-import java.util.*
-import kotlin.Comparator
 
 class ListFragment : Fragment() {
 
@@ -20,7 +17,6 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    @TargetApi(24)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -41,7 +37,7 @@ class ListFragment : Fragment() {
                 // TODO (julianho): Comparator should handle null gracefully.
                 val comparator: Comparator<StationModel> = Comparator { s, t -> s.name!!.compareTo(t.name!!) }
 
-                for ((station, location) in stationMap?.toSortedMap(comparator) ?: TreeMap()) {
+                for ((station, location) in (stationMap ?: hashMapOf()).toSortedMap(comparator)) {
                     // TODO (julianho): Extract variables from the current time, not the first in the day.
                     val aqi = location?.data?.time?.first()?.variables?.aqi?.value
                     eListe.elementer.add(Element(station.name, aqi))
