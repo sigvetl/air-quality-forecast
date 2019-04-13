5950d24c2e30f51fc8c3148013d020ff41257e72
julianho <julianho@student.matnat.uio.no>
2019-04-13 02:34:18 +0200

    Legger til enkel job som senere skal hente Airqualityforecast-informasjon.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/MainActivity.kt
@@ -1,5 +1,11 @@
 package no.uio.ifi.in2000.gruppe55
 
+import android.app.job.JobInfo
+import android.app.job.JobParameters
+import android.app.job.JobScheduler
+import android.app.job.JobService
+import android.content.ComponentName
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
@@ -7,8 +13,30 @@ import android.support.v7.widget.Toolbar
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class AirqualityforecastJobService : JobService() {
+    private var fetchJob: Job? = null
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        fetchJob = launch {
+            jobFinished(params, false)
+        }
+        return false
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        fetchJob?.cancel()
+        return true
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+
+    private val jobScheduler: JobScheduler by lazy {
+        applicationContext.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +49,12 @@ class MainActivity : AppCompatActivity() {
         findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
         //link navigation controller with the bottom navigation menu
         findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+
+        val componentName = ComponentName(this, AirqualityforecastJobService ::class.java)
+        val jobInfo = JobInfo.Builder(0, componentName)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .build()
+
+        jobScheduler.schedule(jobInfo)
     }
 }
