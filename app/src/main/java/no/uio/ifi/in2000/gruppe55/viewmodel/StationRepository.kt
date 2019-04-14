98aa14cc2b69b3edac69d7ec30deca55f41e58d3
julianho <julianho@student.matnat.uio.no>
2019-04-14 21:35:45 +0200

    Henter luftkvalitetslesinger som er gitt for nåtidige klokkeslett.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/viewmodel/StationRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/viewmodel/StationRepository.kt
@@ -3,18 +3,40 @@ package no.uio.ifi.in2000.gruppe55.viewmodel
 import no.uio.ifi.in2000.gruppe55.AirQualityTimeDataModel
 import no.uio.ifi.in2000.gruppe55.Airqualityforecast
 import no.uio.ifi.in2000.gruppe55.StationModel
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 // TODO: Implement caching of requests to reduce unnecessary network usage.
 class StationRepository(private val stationModel: StationModel) {
-    suspend fun at(date: Date): AirQualityTimeDataModel {
+    suspend fun at(date: Date): AirQualityTimeDataModel? {
         val locationModel = Airqualityforecast.main(
             lat = stationModel.latitude,
             lon = stationModel.longitude
         )
 
-        // TODO (julianho): Actually pick the measurement corresponding to the given date & time. Currently just picking
-        // the first measurement in the day for the sake of simplicity.
-        return locationModel.data!!.time!!.first()
+        for (moment in locationModel.data?.time ?: ArrayList()) {
+            // Airqualityforecast times are weird, so simply pick either the first or second date & time and consider
+            // that to be "now". Additionally, all Airqualityforecast times should be within a Norwegian time-zone.
+            val middleDate = SimpleDateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss'Z'",
+                Locale.forLanguageTag("no")
+            ).parse(moment?.from)
+
+            // To find the measurement of the current data & time, one needs to pick times within a range. One potential
+            // range is the previous time plus/minus 30 minutes.
+            var startDate = middleDate.clone() as Date
+            startDate.minutes -= 30
+
+            var endDate = middleDate.clone() as Date
+            endDate.minutes += 30
+
+            // Pick the first measurement within the relevant date & time.
+            if (date.time > startDate.time && date.time < endDate.time) {
+                return moment
+            }
+        }
+
+        return null
     }
 }
