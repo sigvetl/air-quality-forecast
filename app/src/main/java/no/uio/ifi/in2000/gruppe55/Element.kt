bfe762a1abc3dff27a0273e45a1761953d4fdd4b
jarlebm <jarlebm@student.matnat.uio.no>
2019-04-17 13:04:30 +0200

    Loading of info page recycle view(temp)

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/Element.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/Element.kt
@@ -1,6 +1,7 @@
 package no.uio.ifi.in2000.gruppe55
 
 data class Element (val name: String?, val aqi: Double?)
+data class Value (val name: String, val value: Double?, val unit: String?)
 
 object eListe{
     var elementer = mutableListOf(
@@ -9,5 +10,11 @@ object eListe{
         Element("Trondheim", 2.2),
         Element("Oslo", 3.4),
         Element("Skien", 5.3)
+    )
+}
+
+object sListe{
+    var values = mutableListOf(
+        Value("Stov", 2.0, "1")
     )
 }
