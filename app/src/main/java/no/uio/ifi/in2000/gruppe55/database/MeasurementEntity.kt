0f6efc0e3a2d758018ce19215e2d7fd5da976b74
julianho <julianho@student.matnat.uio.no>
2019-04-23 01:45:48 +0200

    Luftkvalitetsverdier har nå stasjoner som "foreign" nøkler.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/database/MeasurementEntity.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/database/MeasurementEntity.kt
@@ -1,7 +1,12 @@
 package no.uio.ifi.in2000.gruppe55.database
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import java.time.OffsetDateTime
 
-@Entity(tableName = "measurement_table", primaryKeys = ["name", "timestamp"])
+@Entity(
+    tableName = "measurement_table",
+    primaryKeys = ["name", "timestamp"],
+    foreignKeys = [ForeignKey(entity = StationEntity::class, parentColumns = ["name"], childColumns = ["name"])]
+)
 data class MeasurementEntity(val name: String, val timestamp: OffsetDateTime, val aqi: Double)
