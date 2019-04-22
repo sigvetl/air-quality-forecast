1c12f5c2c05294b85a531a1335e3cd2752f6b1d4
julianho <julianho@student.matnat.uio.no>
2019-04-23 01:36:44 +0200

    Legger til entity som representerer stasjoner i SQL.

---
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/database/StationEntity.kt
@@ -0,0 +1,7 @@
+package no.uio.ifi.in2000.gruppe55.database
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "station_table")
+data class StationEntity(@PrimaryKey val name: String)
