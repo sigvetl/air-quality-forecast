d1c4c9b64b7d40e69855ac88413e686622b59666
julianho <julianho@student.matnat.uio.no>
2019-04-23 02:19:15 +0200

    Legger til funksjon for å hente luftkvalitetsverdier tatt rundt en viss tidsperiode.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/database/MeasurementDao.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/database/MeasurementDao.kt
@@ -4,11 +4,23 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
+import org.threeten.bp.OffsetDateTime
 
 @Dao
 interface MeasurementDao {
     @get:Query("SELECT * FROM measurement_table")
     val all: List<MeasurementEntity>
+
+    @Query(
+        """
+            SELECT * FROM measurement_table
+            WHERE
+                name = :name AND
+                datetime(:timestamp) > datetime(timestamp, '-30 minutes') AND
+                datetime(:timestamp) < datetime(timestamp, '+30 minutes')
+        """
+    )
+    fun recentTo(name: String, timestamp: OffsetDateTime): List<MeasurementEntity>
 
     @Insert
     fun insert(measurementEntity: MeasurementEntity)
