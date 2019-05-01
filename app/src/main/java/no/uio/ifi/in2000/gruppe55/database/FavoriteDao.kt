4bba75da867c133b84b5ce4bc141609497048611
julianho <julianho@student.matnat.uio.no>
2019-05-01 03:04:42 +0200

    Implementerer enkel reaktiv databaseverdi.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/database/FavoriteDao.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/database/FavoriteDao.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.gruppe55.database
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
@@ -7,6 +8,9 @@ import android.arch.persistence.room.Query
 
 @Dao
 interface FavoriteDao {
+    @get:Query("SELECT * FROM favorite_table")
+    val all: LiveData<List<FavoriteEntity>>
+
     @Query("SELECT * FROM favorite_table WHERE name = :favoriteEntity")
     fun contains(favoriteEntity: FavoriteEntity): Boolean
 
