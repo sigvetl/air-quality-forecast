e045ea9053447228be26b49b2a65df0811843fdd
julianho <julianho@student.matnat.uio.no>
2019-05-01 02:59:25 +0200

    (For nå?) Stasjonområde er ikke lenger fremmednøkkel.

--- app/src/main/java/no/uio/ifi/in2000/gruppe55/database/FavoriteEntity.kt
+++ app/src/main/java/no/uio/ifi/in2000/gruppe55/database/FavoriteEntity.kt
@@ -4,8 +4,5 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
-@Entity(
-    tableName = "favorite_table",
-    foreignKeys = [ForeignKey(entity = StationEntity::class, parentColumns = ["name"], childColumns = ["name"])]
-)
+@Entity(tableName = "favorite_table")
 data class FavoriteEntity(@PrimaryKey val name: String)
